94e778612208956ecd6b2893c77434c46fb1c99c
ckotsidimos <ckotsidimos@gmail.com>
2021-06-24 19:06:40 +0100

    Update tfvars.tf

--- Terraform/tfvars.tf
+++ Terraform/tfvars.tf
@@ -1,7 +1,9 @@
 variable "prefix" {
   description = "The prefix used for all resources in this example"
+  default = ["Kotsisflow"]
 }
 
 variable "location" {
   description = "The Azure location where all resources in this example should be created"
+  default = ["northcentralus"]
 }
